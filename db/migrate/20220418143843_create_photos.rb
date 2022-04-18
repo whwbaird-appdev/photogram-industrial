f6aae4e97c13007b366d816936f3e74046e5e4dd
whw-baird <wbaird@chicagobooth.edu>
2022-04-18 14:44:34 +0000

    Fixed migration issues (hopefully), migrated photos

---
+++ db/migrate/20220418143843_create_photos.rb
@@ -0,0 +1,14 @@
+class CreatePhotos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :photos do |t|
+      t.string :image
+      t.integer :comments_count
+      t.integer :likes_count
+      t.text :caption
+      t.references :owner, null: false, foreign_key: true
+      t.belongs_to  :owner, null: false, foreign_key: { to_table :users }, index: true
+
+      t.timestamps
+    end
+  end
+end
