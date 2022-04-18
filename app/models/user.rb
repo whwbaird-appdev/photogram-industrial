d2292a8b357bd85d633da743372ab370948552f0
whw-baird <wbaird@chicagobooth.edu>
2022-04-18 18:53:33 +0000

    Validations and enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,13 +29,15 @@ class User < ApplicationRecord
   has_many :photos, foreign_key: :owner_id
   has_many :comments, foreign_key: :author_id
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
-  has_many :accepted_sent_follow_requests, -> {where(status: "accepted")}, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :accepted_sent_follow_requests, -> {accepted}, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
-  has_many :accepted_received_follow_requests, -> {where(status: "accepted")}, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :accepted_received_follow_requests, -> {accepted}, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :likes, foreign_key: :fan_id
   has_many :liked_photos, through: :likes, source: :photo
   has_many :leaders, through: :accepted_sent_follow_requests, source: :recipient
   has_many :followers, through: :accepted_received_follow_requests, source: :sender
   has_many :feed, through: :leaders, source: :own_photos
   has_many :discover, through: :leaders, source: :liked_photos
+
+  validates :username, presence: true, uniqueness: true
 end
