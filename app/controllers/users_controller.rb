a46eea81b6f1562197f6ea552c1f14a4b2464341
whw-baird <wbaird@chicagobooth.edu>
2022-04-19 15:15:27 +0000

    Added commenting but not submitting for some reason

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by!(username: params.fetch(:id))
+    @user = User.find_by!(username: params.fetch(:username))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root "photos#index"
-  
-  resources :users, only: :show
 
-  resources :likes
-  resources :follow_requests
-  resources :comments
-  resources :photos
+  
   devise_for :users
+  resources :comments
+  resources :follow_requests
+  resources :likes
+  resources :photos
+  get "/:username" => "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
