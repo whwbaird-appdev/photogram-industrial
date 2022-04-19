e9468fee6c1db5cd009e50b29b9d9a9fc070cf3d
whw-baird <wbaird@chicagobooth.edu>
2022-04-19 15:39:29 +0000

    Fixed login issues, made /users/show nicer

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -57,6 +57,10 @@ class PhotosController < ApplicationController
     end
   end
 
+  def liked
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_photo
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   resources :follow_requests
   resources :likes
   resources :photos
-  get "/:username" => "users#show"
+
+  get ":username/liked" => "photos#liked"
+  get ":username/feed" =>
+  get ":username/followers" => 
+  get ":username/following" =>
+  get ":username" => "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
